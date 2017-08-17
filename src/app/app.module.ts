709840b50df6e70c0e9072a0dfc0793ef6113020
jonnyalexbh <jonnyalex.bh@gmail.com>
2017-08-17 16:55:05 -0500

    create a component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FruitComponent } from './fruit/fruit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FruitComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/fruit/fruit.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fruit',
+  templateUrl: './fruit.component.html'
+})
+
+export class FruitComponent {
+
+  public name_component = 'Fruit Component';
+  public list_fruit = 'Apple, banana, mango and pear';
+
+}
