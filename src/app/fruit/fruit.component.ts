79fa9438bf378756cc8a5b1cbc15d2cf97477b64
jonnyalexbh <jonnyalex.bh@gmail.com>
2017-08-18 12:19:09 -0500

    properties and data types

--- src/app/fruit/fruit.component.ts
+++ src/app/fruit/fruit.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 export class FruitComponent {
   public name_component = 'Fruit Component';
   public list_fruit = 'Apple, banana, mango and pear';
+
+  public name:string = 'jonnyalexbh';
+  public age:number = 26;
+  public adult:boolean = true;
+  public jobs:Array<string> = ['Carpenter', 'bricklayer', 'plumber'];
+  public anyone:any = 99;
 }
