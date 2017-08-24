09951cc987d38cc03c599420a7f29534fae56261
jonnyalexbh <jonnyalex.bh@gmail.com>
2017-08-23 21:34:59 -0500

    create classes, data models, and objects

---
+++ src/app/employee/employee.ts
@@ -0,0 +1,22 @@
+export class Employee {
+
+  /*
+  public name:string;
+  public age:number;
+
+  constructor(name, age){
+  this.name = name;
+  this.age = age;
+}
+
+*/
+
+constructor(
+  public name:string,
+  public age:number,
+  public position:string,
+  public hired:boolean
+){
+}
+
+}
