e657aefdd623d7b5afd43889ab1024f443ef0d1a
jonnyalexbh <jonnyalex.bh@gmail.com>
2017-08-28 17:30:34 -0500

    routes and parameters by the url

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'contact',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 
 export class ContactComponent{
   public title = 'Web contact page';
+  public parameter;
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router
+  ){}
+
+  ngOnInit(){
+    this._route.params.forEach((params : Params) => {
+      this.parameter = params['page'];
+      console.log(params);
+    });
+  }
+
 }
