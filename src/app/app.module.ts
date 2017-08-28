b68f9db43879c9f40d11fa7d603f30969748a708
jonnyalexbh <jonnyalex.bh@gmail.com>
2017-08-28 16:08:01 -0500

    configure routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { routing, appRoutingProviders} from './app.routing';
 
 import { AppComponent } from './app.component';
 import { FruitComponent } from './fruit/fruit.component';
@@ -16,9 +17,10 @@ import { EmployeeComponent } from './employee/employee.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
-  providers: [],
+  providers: [appRoutingProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,18 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule} from '@angular/router';
+
+// Import components
+
+import { EmployeeComponent } from './employee/employee.component';
+import { FruitComponent } from './fruit/fruit.component';
+
+const appRoutes: Routes = [
+  {path: '', component: EmployeeComponent },
+  {path: 'employee', component: EmployeeComponent },
+  {path: 'fruit', component: FruitComponent },
+  {path: '**', component: EmployeeComponent }
+];
+
+export const appRoutingProviders: any[] = [];
+
+export const routing:ModuleWithProviders = RouterModule.forRoot(appRoutes);
