152b1ca7859f5da91d2cfe03488a0d7ac1f10022
jonnyalexbh <jonnyalex.bh@gmail.com>
2017-08-29 12:31:21 -0500

    create component and path

---
+++ src/app/layouts/layout.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector : 'layout',
+  templateUrl: './layout.component.html'
+})
+
+export class LayoutComponent{
+
+  public title;
+  public administrator;
+
+  constructor(){
+    this.title = 'Templates ngTemplate in angular';
+    this.administrator = true;
+  }
+
+}
