461e369509ef3b284072741ecc7265d1ab673111
jonnyalexbh <jonnyalex.bh@gmail.com>
2017-08-28 22:16:29 -0500

    exercise with services

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,14 +9,25 @@ import { ClothesService } from '../services/clothes.services';
 
 export class HomeComponent{
   public title = 'Homepage';
+  public clothing_list:Array<string>;
+  public hold_save:string;
 
   constructor(
     private _clothesService: ClothesService)
     {}
 
     ngOnInit(){
-      this._clothesService.test('Jean');
-      console.log(this._clothesService.test('jean Colombia'));
+      this.clothing_list = this._clothesService.getClothes();
+      console.log(this.clothing_list);
+    }
+
+    savePledge(){
+      this._clothesService.addClothes(this.hold_save);
+      this.hold_save = null;
+    }
+
+    removeGarment(index:number){
+      this._clothesService.removeGarment(index);
     }
 
   }
--- src/app/services/clothes.services.ts
+++ src/app/services/clothes.services.ts
@@ -3,9 +3,24 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class ClothesService{
   public namePledge = 'jeans';
+  public clothingCollection = ['White pants', 'Red shirt'];
 
   test(name){
     return name;
+  }
+
+  addClothes(name_pledge:string):Array<string>{
+    this.clothingCollection.push(name_pledge);
+    return this.getClothes();
+  }
+
+  removeGarment(index:number){
+    this.clothingCollection.splice(index, 1);
+    return this.getClothes();
+  }
+
+  getClothes():Array<string>{
+    return this.clothingCollection;
   }
 
 }
